2a605cfa7cc708beab45c01bdd20ff414982db0e
Dmitrii Barsukov <analitik.asdf@gmail.com>
2021-06-21 13:13:32 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c8c5e05e60407b1cda3334558655c9b9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c6f40693-1d0f-4771-a029-13be14a946a0'
+};
