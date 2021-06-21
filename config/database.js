e7df9b8936fbde6edeb66a0860b7bb16827ed0fd
Dmitrii Barsukov <analitik.asdf@gmail.com>
2021-06-21 14:09:48 +0300

    test

--- config/database.js
+++ config/database.js
@@ -1,15 +1,36 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME'),
+//         username: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
+        host: env('DATABASE_HOST', 'toxin.tz2bb.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        database: env('DATABASE_NAME', 'deploy-strapi'),
+        username: env('DATABASE_USERNAME', 'user42'),
+        password: env('DATABASE_PASSWORD', '9601030615'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
